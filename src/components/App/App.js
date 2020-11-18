7163741fe61574881cf04c8657cbe2eb6a003ba8
Bradley O'Leary <bradley_oleary@outlook.com>
2020-11-18 15:16:00 -0500

    Ex 5 - Routing

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,13 +1,29 @@
 import React from "react";
-import { BrowserRouter as Router } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
+
 import GlobalStyles from "./GlobalStyles";
+import ArtistRoute from "../ArtistRoute";
+
+const DEFAULT_ARTIST_ID = "5INjqkS1o8h1imAzPqGZBb";
 
 const App = () => {
   return (
-    <>
+    <Router>
       <GlobalStyles />
-      <Router>TODO</Router>
-    </>
+      <Switch>
+        <Route exact path="/">
+          <Redirect to={`artist/${DEFAULT_ARTIST_ID}`} />
+        </Route>
+        <Route path="/artist/:id">
+          <ArtistRoute />
+        </Route>
+      </Switch>
+    </Router>
   );
 };
 
---
+++ src/components/ArtistRoute.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ArtistRoute = () => {
+  return <div>Artist Page</div>;
+};
+
+export default ArtistRoute;
